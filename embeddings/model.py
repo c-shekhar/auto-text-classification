a6ac7f514baef461ea737be33c3b895d0ec79794
c-shekhar <chandrashekhar1503@gmail.com>
2019-02-20 20:47:47 +0530

    embeddings module

---
+++ embeddings/model.py
@@ -0,0 +1,38 @@
+from imports import word2vec
+
+class W2VModel(object):
+	
+	def __init__(self, num_features = 300, min_word_count = 40, 
+				num_workers = 4, context = 10, downsampling = 1e-3):
+
+		# Set values for various parameters
+		self.num_features = num_features    # Word vector dimensionality                      
+		self.min_word_count = min_word_count   # Minimum word count                        
+		self.num_workers = num_workers       # Number of threads to run in parallel
+		self.context = context          # Context window size
+		self.downsampling = downsampling   # Downsample setting for frequent words
+		self.model = None
+
+	def train(self, sentences, init_sims=True, loglevel='INFO'):
+		import logging
+		if loglevel == 'INFO':
+			lv = logging.INFO
+		logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+			level=lv)
+		print('###########################################')
+		print("########## Training W2V model #############")
+		print('###########################################')
+		model = word2vec.Word2Vec(sentences, workers=self.num_workers,
+								size=self.num_features, min_count=self.min_word_count,
+								window=self.context, sample=self.downsampling)
+		
+		# If we don't plan to train the model any further, calling 
+		# init_sims will make the model much more memory-efficient.
+		model.init_sims(replace=init_sims)
+		self.model = model
+		return model
+
+	def save(self, model_name):
+		# model_name = "toxic_300features_40minwords_10context"
+		self.model.save(model_name)
+
---
+++ embeddings/vectorize.py
@@ -0,0 +1,55 @@
+from imports import *
+from preprocess.converters import Converter
+from imports import Word2Vec
+
+class Vectorize(object):
+	def __init__(self):
+		self.bow_model = None
+		self.tf_idf_model = None
+		self.w2v_model = None
+		# self.converter = Converter()
+
+
+	def to_count_vector(self, text_rows):
+		vectorizer = CountVectorizer(analyzer='word',
+									tokenizer=None,
+									preprocessor=None,
+									stop_words=None,
+									max_features = 5000)
+		vec_tfms = vectorizer.fit_transform(text_rows)
+		return vec_tfms
+
+
+	def to_tfidf_vector(self, text_rows):
+		vectorizer = TfidfVectorizer(analyzer='word',
+									tokenizer=None,
+									preprocessor=None,
+									stop_words=None,
+									max_features = 5000)
+
+		vec_tfms = vectorizer.fit_transform(text_rows)
+		return vec_tfms
+
+
+	def to_w2v(self, words, model, vocab, num_features, remove_stopwords):
+		feature_vec = np.zeros((num_features,),dtype='float32')
+		nwords = 0
+		for wd in words:
+			if wd in vocab:
+				nwords += 1
+				_vec = model.wv[wd]
+				feature_vec = np.add(feature_vec,_vec)
+		feature_vec = np.divide(feature_vec,nwords)
+		return feature_vec
+
+
+	def to_word_vector(self, words_rows, model_path=None, num_features=300, remove_stopwords=True):
+		model = Word2Vec.load(model_path)
+		vocab = model.wv.index2word
+		clean_train_rows = []
+		for i,row in enumerate(words_rows):
+			review_feature_vec = self.to_w2v(row,model,vocab,num_features,remove_stopwords)
+			clean_train_rows.append(review_feature_vec)
+			if ((i + 1) % 1000 == 0):
+				print(f"{i+1} train rows vectorized...")
+		return np.array(clean_train_rows)
